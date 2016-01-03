7be5e279dd77ab2184bf7070858c3635ebf2a49b
pemrouz <p3pedram@hotmail.com>
2016-01-03 21:36:55 +0000

    allow arbitrary attr placement

--- test.js
+++ test.js
@@ -6,8 +6,9 @@ var expect = require('chai').expect
 
 describe('el', function() {
 
-  it('should append only one div by default', function() {
+  it('should create element from selector string', function() {
     var node = el('foo-bar.classA.classB[attr1=value1][attr2=value2]')
+    expect(node.nodeName).to.be.eql('FOO-BAR')
     expect(node.className).to.be.eql('classA classB')
     expect(attr(node, 'attr1')).to.be.eql('value1')
     expect(attr(node, 'attr2')).to.be.eql('value2')
@@ -17,6 +18,15 @@ describe('el', function() {
     var node = el('foo-bar.classA.classB[attr1=value1][attr2=value2]')
     expect(node.toString()).to.be.eql('foo-bar.classA.classB')
   })
+
+  it('should allow arbitrary attr placement', function() {
+    var node = el('foo-bar[attr1=value1][attr2=value2].classA.classB')
+    expect(node.nodeName).to.be.eql('FOO-BAR')
+    expect(node.className).to.be.eql('classA classB')
+    expect(attr(node, 'attr1')).to.be.eql('value1')
+    expect(attr(node, 'attr2')).to.be.eql('value2')
+  })
+
 
 })
 
